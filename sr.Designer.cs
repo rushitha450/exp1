4c654b4635e3ceff0d0fb7c4d946e88536af815e
rushitha450 <43805364+rushitha450@users.noreply.github.com>
2019-01-05 20:13:29 +0530

    Add files via upload

---
+++ sr.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace pg
+{
+    partial class sr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.gph = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ph = new System.Windows.Forms.TextBox();
+            this.address = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.gb1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.gb2 = new System.Windows.Forms.GroupBox();
+            this.nonveg = new System.Windows.Forms.RadioButton();
+            this.veg = new System.Windows.Forms.RadioButton();
+            this.img = new System.Windows.Forms.PictureBox();
+            this.imgAdr = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dob = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gb1.SuspendLayout();
+            this.gb2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gph
+            // 
+            this.gph.Location = new System.Drawing.Point(184, 390);
+            this.gph.Name = "gph";
+            this.gph.Size = new System.Drawing.Size(100, 22);
+            this.gph.TabIndex = 1;
+            this.gph.TextChanged += new System.EventHandler(this.gph_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 393);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Guardian phone No";
+            // 
+            // ph
+            // 
+            this.ph.Location = new System.Drawing.Point(185, 360);
+            this.ph.Name = "ph";
+            this.ph.Size = new System.Drawing.Size(100, 22);
+            this.ph.TabIndex = 6;
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(180, 119);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(100, 22);
+            this.address.TabIndex = 7;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(180, 85);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 22);
+            this.name.TabIndex = 8;
+            // 
+            // gb1
+            // 
+            this.gb1.Controls.Add(this.radioButton3);
+            this.gb1.Controls.Add(this.radioButton2);
+            this.gb1.Controls.Add(this.radioButton1);
+            this.gb1.Location = new System.Drawing.Point(83, 149);
+            this.gb1.Name = "gb1";
+            this.gb1.Size = new System.Drawing.Size(203, 60);
+            this.gb1.TabIndex = 9;
+            this.gb1.TabStop = false;
+            this.gb1.Text = "Share";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(125, 25);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(37, 21);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(39, 25);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(37, 21);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(82, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 21);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "2";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // gb2
+            // 
+            this.gb2.Controls.Add(this.nonveg);
+            this.gb2.Controls.Add(this.veg);
+            this.gb2.Location = new System.Drawing.Point(83, 219);
+            this.gb2.Name = "gb2";
+            this.gb2.Size = new System.Drawing.Size(200, 100);
+            this.gb2.TabIndex = 10;
+            this.gb2.TabStop = false;
+            this.gb2.Text = "Food preference";
+            // 
+            // nonveg
+            // 
+            this.nonveg.AutoSize = true;
+            this.nonveg.Location = new System.Drawing.Point(7, 64);
+            this.nonveg.Name = "nonveg";
+            this.nonveg.Size = new System.Drawing.Size(82, 21);
+            this.nonveg.TabIndex = 1;
+            this.nonveg.TabStop = true;
+            this.nonveg.Text = "Non veg";
+            this.nonveg.UseVisualStyleBackColor = true;
+            this.nonveg.CheckedChanged += new System.EventHandler(this.nonveg_CheckedChanged);
+            // 
+            // veg
+            // 
+            this.veg.AutoSize = true;
+            this.veg.Location = new System.Drawing.Point(7, 22);
+            this.veg.Name = "veg";
+            this.veg.Size = new System.Drawing.Size(54, 21);
+            this.veg.TabIndex = 0;
+            this.veg.TabStop = true;
+            this.veg.Text = "Veg";
+            this.veg.UseVisualStyleBackColor = true;
+            this.veg.CheckedChanged += new System.EventHandler(this.veg_CheckedChanged);
+            // 
+            // img
+            // 
+            this.img.Location = new System.Drawing.Point(431, 119);
+            this.img.Name = "img";
+            this.img.Size = new System.Drawing.Size(195, 235);
+            this.img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img.TabIndex = 11;
+            this.img.TabStop = false;
+            // 
+            // imgAdr
+            // 
+            this.imgAdr.Location = new System.Drawing.Point(431, 81);
+            this.imgAdr.Name = "imgAdr";
+            this.imgAdr.Size = new System.Drawing.Size(114, 22);
+            this.imgAdr.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(551, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Choose";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dob
+            // 
+            this.dob.Location = new System.Drawing.Point(185, 331);
+            this.dob.Name = "dob";
+            this.dob.Size = new System.Drawing.Size(100, 22);
+            this.dob.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(127, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "DOB";
+            // 
+            // sr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dob);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imgAdr);
+            this.Controls.Add(this.img);
+            this.Controls.Add(this.gb2);
+            this.Controls.Add(this.gb1);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.ph);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gph);
+            this.Controls.Add(this.button1);
+            this.Name = "sr";
+            this.Text = "sr";
+            this.Load += new System.EventHandler(this.sr_Load);
+            this.gb1.ResumeLayout(false);
+            this.gb1.PerformLayout();
+            this.gb2.ResumeLayout(false);
+            this.gb2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox gph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ph;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.GroupBox gb1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox gb2;
+        private System.Windows.Forms.RadioButton nonveg;
+        private System.Windows.Forms.RadioButton veg;
+        private System.Windows.Forms.PictureBox img;
+        private System.Windows.Forms.TextBox imgAdr;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox dob;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ sr.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace pg
+{
+    public partial class sr : Form
+    {
+        string food;
+        int share;
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\kranthi\source\repos\pg\pg\Database1.mdf;Integrated Security=True");
+        public sr()
+        {
+            InitializeComponent();
+        }
+
+        private void sr_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if(op.ShowDialog()==DialogResult.OK)
+            {
+                imgAdr.Text = op.FileName;
+                img.ImageLocation = imgAdr.Text;
+            }
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            con.Open();
+            string com = "INSERT INTO studentR (name,address,share,mobile,gnumber,food,img,dob,room) VALUES('"+name.Text+"','"+address.Text+"')";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            share = 1;
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            share = 2;
+            
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            share = 3;
+        }
+
+        private void veg_CheckedChanged(object sender, EventArgs e)
+        {
+            food = "veg";
+        }
+
+        private void nonveg_CheckedChanged(object sender, EventArgs e)
+        {
+            food = "Non-veg";
+        }
+
+        private void gph_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
